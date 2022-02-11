c8303468398e492ed150a6f131887bcad62bc837
Jordi Mas <jmas@softcatala.org>
2022-02-11 10:29:56 +0100

    Commit

--- eval/eval.py
+++ eval/eval.py
@@ -21,13 +21,12 @@ if __name__ == "__main__":
     print(f"Evaluation")
 
     VALID_LABELS = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O", '-O', '?O', '?U']
-    #valid_labels = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O", '-O', '?O', '?U']
     model = NERModel("bert", "outputs/", labels=VALID_LABELS,
                           args={"silent": True, "max_seq_length": 512})
 
 
     result = model.eval_model("rpunct_test_set.txt", output_dir = "eval_model/")
-    print(f"result: {result}")
+    #print(f"result: {result}")
 
     with open('eval-results.txt', 'w') as fp:
         fp.write(result)
