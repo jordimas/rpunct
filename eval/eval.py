88309bdadc70fcfcc3a13d9efa6dd36a5220e147
Jordi Mas <jmas@softcatala.org>
2022-02-11 10:16:31 +0100

    Commit

--- eval/eval.py
+++ eval/eval.py
@@ -1,12 +1,34 @@
 from simpletransformers.ner import NERModel
 
+'''
+def train_model():
+    """
+    Trains simpletransformers model
+    """
+    # Create a NERModel
+    model = NERModel("bert", "bert-base-uncased",
+                     args={"overwrite_output_dir": True,
+                           "num_train_epochs": 3,
+                           "max_seq_length": 512,
+                           "lazy_loading": True},
+                     labels=VALID_LABELS)
+
+    # # Train the model
+    steps, tr_details = model.train_model('rpunct_train_set.txt')
+'''
+
 if __name__ == "__main__":
     print(f"Evaluation")
 
-    valid_labels = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O", '-O', '?O', '?U']
-    model = NERModel("bert", "outputs/", labels=valid_labels,
+    VALID_LABELS = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O"
+    #valid_labels = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O", '-O', '?O', '?U']
+    model = NERModel("bert", "outputs/", labels=VALID_LABELS,
                           args={"silent": True, "max_seq_length": 512})
 
 
     result = model.eval_model("rpunct_test_set.txt", output_dir = "eval_model/")
     print(f"result: {result}")
+
+    with open('eval-results.txt', 'w') as fp:
+        fp.write(result)
+
