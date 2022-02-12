de692e226b5c47c9597f24edc05f518df4c20efe
Jordi Mas <jmas@softcatala.org>
2022-02-12 14:59:07 +0100

    Out

--- eval/eval.py
+++ eval/eval.py
@@ -21,11 +21,10 @@ if __name__ == "__main__":
     print(f"Evaluation")
 
     VALID_LABELS = ['OU', 'OO', '.O', '!O', ',O', '.U', '!U', ',U', ':O', ';O', ':U', "'O", '-O', '?O', '?U']
-    model = NERModel("bert", "outputs/", labels=VALID_LABELS,
-                          args={"silent": True, "max_seq_length": 512})
+    model = NERModel("bert", "outputs/", labels=VALID_LABELS, args={"silent": True, "max_seq_length": 512})
 
 
-    result = model.eval_model("rpunct_test_set.txt", output_dir = "eval-localmodel/")
+    result, model_outputs, pred_list = model.eval_model("rpunct_test_set.txt", output_dir = "eval-localmodel/")
 
     model = NERModel("bert", "felflare/bert-restore-punctuation", labels=VALID_LABELS,
                           args={"silent": True, "max_seq_length": 512})
