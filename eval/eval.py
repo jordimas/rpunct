10c393b45a8120f7456287f67ab094257f7c9883
Jordi Mas <jmas@softcatala.org>
2022-02-13 18:49:10 +0100

    Fix

--- eval/eval.py
+++ eval/eval.py
@@ -1,21 +1,5 @@
 from simpletransformers.ner import NERModel
 
-'''
-def train_model():
-    """
-    Trains simpletransformers model
-    """
-    # Create a NERModel
-    model = NERModel("bert", "bert-base-uncased",
-                     args={"overwrite_output_dir": True,
-                           "num_train_epochs": 3,
-                           "max_seq_length": 512,
-                           "lazy_loading": True},
-                     labels=VALID_LABELS)
-
-    # # Train the model
-    steps, tr_details = model.train_model('rpunct_train_set.txt')
-'''
 
 if __name__ == "__main__":
     print(f"Evaluation")
