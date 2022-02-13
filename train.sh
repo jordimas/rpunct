5d8ec86727be3a2d724d4b2e1c4fe07b0f204915
Jordi Mas <jmas@softcatala.org>
2022-02-13 15:20:45 +0100

    Train

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 rm -r -f runs/
 rm -r -f outputs/
-python training/train.py 
+python3 training/train.py 
 
 echo "Test training score"
 cat test_training/eval_results.txt
 
 echo "Flores eval score"
-cat eval_training_flores//eval_results.txt
+cat eval_training_flores/eval_results.txt
 
