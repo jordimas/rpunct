8b67b4f03e721737a0ea579bfcc6b5e19e6dd348
Jordi Mas <jmas@softcatala.org>
2022-02-15 09:31:46 +0100

    Fix

--- pack.sh
+++ pack.sh
@@ -1,9 +1,9 @@
 OUTDIR=dist/
 
 rm -r -f $OUTDIR
-mkdir -r $OUTDIR/outputs
+mkdir -p $OUTDIR/outputs
 cp outputs/* $OUTDIR/outputs
-cp rpunt/punctuate.py $OUTDIR
+cp rpunct/punctuate.py $OUTDIR
 cp tests/* $OUTDIR
 cp eval/prompt.py $OUTDIR
 echo pushd $OUTDIR && zip ../rpunct-ca.zip -r * && popd
