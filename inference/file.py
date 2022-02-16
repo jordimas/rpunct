007fa9d0ef6238d4c01ffaf41aca38b04cdfc7a1
Jordi Mas <jmas@softcatala.org>
2022-02-16 05:52:27 +0100

    Fix

--- inference/file.py
+++ inference/file.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import datetime
 
 sys.path.append('rpunct/')
 
@@ -11,18 +10,13 @@ from simpletransformers.ner import NERModel
 from punctuate import RestorePuncts
 
 if __name__ == "__main__":
-    start_time = datetime.datetime.now()
-
-    with open('inference/sample_text.txt', 'r') as fp:
-        test_sample = fp.readlines()
-
     punct_model = RestorePuncts(model="outputs/")
-    # read test file
-    while True:
-        text = input("text:")
-    # predict text and print
-        punctuated = punct_model.punctuate(text)
-        print(punctuated)
+    for line in test_sample:
+        line = line.strip()
+        if len(line) == 0:
+            continue
 
-    s = 'Time used: {0}'.format(datetime.datetime.now() - start_time)
-    print(s)
+        print(f"source: '{line}'")
+        punctuated = punct_model.punctuate(line)
+        print(f"result: '{punctuated}'")
+        print("--")
--- inference/prompt.py
+++ inference/prompt.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import datetime
 
 sys.path.append('rpunct/')
 
@@ -10,13 +11,18 @@ from simpletransformers.ner import NERModel
 from punctuate import RestorePuncts
 
 if __name__ == "__main__":
-    punct_model = RestorePuncts(model="outputs/")
-    for line in test_sample:
-        line = line.strip()
-        if len(line) == 0:
-            continue
+    start_time = datetime.datetime.now()
 
-        print(f"source: '{line}'")
-        punctuated = punct_model.punctuate(line)
-        print(f"result: '{punctuated}'")
-        print("--")
+    with open('inference/sample_text.txt', 'r') as fp:
+        test_sample = fp.readlines()
+
+    punct_model = RestorePuncts(model="outputs/")
+    # read test file
+    while True:
+        text = input("text:")
+    # predict text and print
+        punctuated = punct_model.punctuate(text)
+        print(punctuated)
+
+    s = 'Time used: {0}'.format(datetime.datetime.now() - start_time)
+    print(s)
