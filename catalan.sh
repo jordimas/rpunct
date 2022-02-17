2f46e1268cdbbc9227d9ee1d024a6a04608bef2d
Jordi Mas <jmas@softcatala.org>
2022-02-17 16:19:25 +0100

    Fix

--- catalan.sh
+++ catalan.sh
@@ -1,3 +1,5 @@
+wget https://www.softcatala.org/pub/softcatala/parallel-corpus-search/eng-cat.cat.zip
+unzip eng-cat.cat.zip
 grep "\.\|," tgt.txt  | grep '.\{230\}' > catalan_train.txt
 wc -l catalan_train.txt
 
